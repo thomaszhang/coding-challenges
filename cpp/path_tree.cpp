53cfd9abf497b7587232305f945a58c9d1d3b2ac
Thomas Zhang <xthomaszhang@icloud.com>
2020-07-22 14:31:04 -0400

    Added LC#112 in C++.

---
+++ cpp/path_tree.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        // Base case
+        if (root == NULL)
+            return false;
+        // Leaf exists
+        if (root->left == NULL && root->right == NULL && root->val == sum)
+            return true;
+        // Recursive case (split subtrees)
+        return hasPathSum(root->right, sum-root->val) || hasPathSum(root->left, sum-root->val);
+    }
+};
