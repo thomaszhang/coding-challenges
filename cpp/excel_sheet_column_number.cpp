f43cf82cfad8290540be1fd37aa7fecd67319a4a
Thomas Zhang <xthomaszhang@icloud.com>
2020-07-26 22:18:07 -0400

    Added LC#171 in C++.

---
+++ cpp/excel_sheet_column_number.cpp
@@ -0,0 +1,12 @@
+// O(N)
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int num = 0;
+        for (int i = 0; i < s.size(); i++) {
+            num = num*26;
+            num += s[i] - '@'; // 'A' - '@' = 1
+        }
+        return num;
+    }
+};
