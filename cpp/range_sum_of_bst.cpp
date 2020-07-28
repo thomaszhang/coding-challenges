9a42970c23b697be7fed254253379047313548de
Thomas Zhang <xthomaszhang@icloud.com>
2020-07-27 20:12:32 -0400

    Updated LC#938 in C++.

--- cpp/range_sum_of_bst.cpp
+++ cpp/range_sum_of_bst.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Without pruning (more performant?)
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int L, int R) {
@@ -20,6 +22,31 @@ public:
         if (root->val >= L && root->val <= R) {
             return root->val + rangeSumBST(root->left, L, R) + rangeSumBST(root->right, L, R);
         }
+        return rangeSumBST(root->left, L, R) + rangeSumBST(root->right, L, R);
+    }
+};
+
+// With pruning
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int L, int R) {
+        // Base case
+        if (root == NULL) {
+            return 0;
+        }
+        // Recursive 
+        if (root->val >= L && root->val <= R) { 
+            return root->val + rangeSumBST(root->left, L, R) + rangeSumBST(root->right, L, R);
+        }
+        // Prune left subtree
+        if (root->val < L) {
+            return rangeSumBST(root->right, L, R);
+        }
+        // Prune right subtree
+        if (root->val > R) {
+            return rangeSumBST(root->left, L, R);
+        }
+        // Default
         return rangeSumBST(root->left, L, R) + rangeSumBST(root->right, L, R);
     }
 };
